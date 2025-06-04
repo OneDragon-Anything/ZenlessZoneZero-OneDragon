d7257aa2474d11dc8dc2e58ba49f785b99729891
A-nony-mous <namesblanks@gmail.com>
2025-06-04 17:55:29 +0800

    feat!: remove ⬆️ ⬇️ button of mission list
    BREAKING CHANGE: now can use drag to adjust mission list sequence only

--- src/one_dragon_qt/widgets/setting_card/app_run_card.py
+++ src/one_dragon_qt/widgets/setting_card/app_run_card.py
@@ -12,8 +12,8 @@ from one_dragon.utils.i18_utils import gt
 
 class AppRunCard(MultiPushSettingCard):
 
-    move_up = Signal(str)
-    move_down = Signal(str)
+    # move_up = Signal(str)
+    # move_down = Signal(str)
     run = Signal(str)
     switched = Signal(str, bool)
     drag_started = Signal(str)
@@ -34,13 +34,13 @@ class AppRunCard(MultiPushSettingCard):
         self.drag_btn = TransparentToolButton(FluentIcon.MENU, None)
         self.drag_btn.setToolTip('按住拖拽以调整顺序')
 
-        self.move_up_btn = TransparentToolButton(FluentIcon.UP, None)
-        self.move_up_btn.clicked.connect(self._on_move_up_clicked)
-        self.move_up_btn.setToolTip('向上移动一位')
-
-        self.move_down_btn = TransparentToolButton(FluentIcon.DOWN, None)
-        self.move_down_btn.clicked.connect(self._on_move_down_clicked)
-        self.move_down_btn.setToolTip('向下移动一位')
+        # self.move_up_btn = TransparentToolButton(FluentIcon.UP, None)
+        # self.move_up_btn.clicked.connect(self._on_move_up_clicked)
+        # self.move_up_btn.setToolTip('向上移动一位')
+        #
+        # self.move_down_btn = TransparentToolButton(FluentIcon.DOWN, None)
+        # self.move_down_btn.clicked.connect(self._on_move_down_clicked)
+        # self.move_down_btn.setToolTip('向下移动一位')
 
         self.run_btn = TransparentToolButton(FluentIcon.PLAY, None)
         self.run_btn.clicked.connect(self._on_run_clicked)
@@ -53,7 +53,7 @@ class AppRunCard(MultiPushSettingCard):
 
         MultiPushSettingCard.__init__(
             self,
-            btn_list=[self.drag_btn, self.move_up_btn, self.move_down_btn, self.run_btn, self.switch_btn],
+            btn_list=[self.drag_btn, self.run_btn, self.switch_btn],
             icon=FluentIcon.GAME,
             title=self.app.op_name,
             parent=parent,
@@ -144,19 +144,19 @@ class AppRunCard(MultiPushSettingCard):
             icon = FluentIcon.INFO
         self.iconLabel.setIcon(icon)
 
-    def _on_move_up_clicked(self) -> None:
-        """
-        向上移动运行顺序
-        :return:
-        """
-        self.move_up.emit(self.app.app_id)
-
-    def _on_move_down_clicked(self) -> None:
-        """
-        向下移动运行顺序
-        :return:
-        """
-        self.move_down.emit(self.app.app_id)
+    # def _on_move_up_clicked(self) -> None:
+    #     """
+    #     向上移动运行顺序
+    #     :return:
+    #     """
+    #     self.move_up.emit(self.app.app_id)
+    #
+    # def _on_move_down_clicked(self) -> None:
+    #     """
+    #     向下移动运行顺序
+    #     :return:
+    #     """
+    #     self.move_down.emit(self.app.app_id)
 
     def _on_run_clicked(self) -> None:
         """
@@ -184,8 +184,8 @@ class AppRunCard(MultiPushSettingCard):
     def setDisabled(self, arg__1: bool) -> None:
         MultiPushSettingCard.setDisabled(self, arg__1)
         self.drag_btn.setDisabled(arg__1)
-        self.move_up_btn.setDisabled(arg__1)
-        self.move_down_btn.setDisabled(arg__1)
+        # self.move_up_btn.setDisabled(arg__1)
+        # self.move_down_btn.setDisabled(arg__1)
         self.run_btn.setDisabled(arg__1)
         self.switch_btn.setDisabled(arg__1)
 
