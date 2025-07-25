0e5d3a1fb4254dc453ce912b79981c0a1edb28d0
Qian23333 <119576779+Qian23333@users.noreply.github.com>
2025-07-25 11:30:54 +0800

    删除冲突部分

--- src/zzz_od/application/charge_plan/charge_plan_app.py
+++ src/zzz_od/application/charge_plan/charge_plan_app.py
@@ -269,7 +269,6 @@ class ChargePlanApp(ZApplication):
     @node_from(from_name='专业挑战室', status=ExpertChallenge.STATUS_CHARGE_NOT_ENOUGH)
     @node_from(from_name='恶名狩猎', status=NotoriousHunt.STATUS_CHARGE_NOT_ENOUGH)
     @node_from(from_name='传送', status='选择失败')
-    @node_from(from_name='传送', success=False)
     @operation_node(name='电量不足')
     def charge_not_enough(self) -> OperationRoundResult:
         # 检查自动回复电量设置
