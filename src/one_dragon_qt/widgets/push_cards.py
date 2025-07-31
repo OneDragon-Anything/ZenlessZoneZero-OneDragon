74c59ba340398bcaaab0ecc30ddda20b56ffe07e
niuhuan <niuhuancn@outlook.com>
2025-07-31 12:20:55 +0800

    refactor: rebase on dev-push

--- src/one_dragon_qt/widgets/push_cards.py
+++ src/one_dragon_qt/widgets/push_cards.py
@@ -492,6 +492,12 @@ class PushCards:
             "required": True
         },
         {
+            "var_suffix": "STARTTLS",
+            "title": "是否使用 STARTTLS",
+            "icon": FluentIcon.PEOPLE,
+            "placeholder": "true 或 false"  
+        },
+        {
             "var_suffix": "EMAIL",
             "title": "收发件邮箱",
             "icon": "CLOUD",
