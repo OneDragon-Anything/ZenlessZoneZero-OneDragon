c5644482955806504832767fe8f80b53cb70e221
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2025-10-22 14:56:38 +0800

    refactor: 优化资源下载界面加载流程

--- src/one_dragon_qt/view/setting/resource_download_interface.py
+++ src/one_dragon_qt/view/setting/resource_download_interface.py
@@ -50,6 +50,8 @@ class ResourceDownloadInterface(VerticalScrollInterface):
         group.addSettingCard(self.launcher_opt)
 
         self.ocr_opt = OnnxModelDownloadCard(ctx=self.ctx, icon=FluentIcon.GLOBE, title='OCR识别')
+        self.ocr_opt.set_options_by_list(get_ocr_opts())
+        self.ocr_opt.set_value_by_save_file_name(f'{self.ctx.model_config.ocr}.zip')
         self.ocr_opt.value_changed.connect(self.on_ocr_changed)
         self.ocr_opt.gpu_changed.connect(self.on_ocr_gpu_changed)
         group.addSettingCard(self.ocr_opt)
@@ -72,15 +74,11 @@ class ResourceDownloadInterface(VerticalScrollInterface):
 
     def init_ocr_opts(self) -> None:
         self.ocr_opt.blockSignals(True)
-        self.ocr_opt.set_options_by_list(get_ocr_opts())
-        self.ocr_opt.set_value_by_save_file_name(f'{self.ctx.model_config.ocr}.zip')
         self.ocr_opt.gpu_opt.setChecked(self.ctx.model_config.ocr_gpu)
-        self.ocr_opt.check_and_update_display()
         self.ocr_opt.blockSignals(False)
 
     def on_ocr_changed(self, index: int, value: CommonDownloaderParam) -> None:
         self.ctx.model_config.ocr = value.save_file_name[:-4]
-        self.ocr_opt.check_and_update_display()
 
     def on_ocr_gpu_changed(self, value: bool) -> None:
         self.ctx.model_config.ocr_gpu = value
