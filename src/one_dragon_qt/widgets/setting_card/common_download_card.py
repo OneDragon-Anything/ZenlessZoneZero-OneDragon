7a48816ff76aa3a23217bb8839159f6b3f6352e6
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2025-10-22 10:34:18 +0800

    perf: 子线程完成后再替换 downloader

--- src/one_dragon_qt/widgets/setting_card/common_download_card.py
+++ src/one_dragon_qt/widgets/setting_card/common_download_card.py
@@ -132,11 +132,18 @@ class CommonDownloaderSettingCard(MultiPushSettingCard):
         # 如果已有线程对象，只替换 downloader；否则创建新线程
         if self.download_runner is not None:
             if self.download_runner.isRunning():
-                # 线程正在运行，先停止再替换
-                self.download_runner.quit()
-                self.download_runner.wait()
-            # 替换 downloader 对象
-            self.download_runner.downloader = self.downloader
+                # 避免阻塞 UI：在线程结束时再替换 downloader
+                def _swap_downloader_on_finish(*_args):
+                    try:
+                        self.download_runner.downloader = self.downloader
+                    finally:
+                        try:
+                            self.download_runner.finished.disconnect(_swap_downloader_on_finish)
+                        except Exception:
+                            pass
+                self.download_runner.finished.connect(_swap_downloader_on_finish)
+            else:
+                self.download_runner.downloader = self.downloader
         else:
             # 首次创建线程对象
             self.download_runner = DownloadRunner(self.ctx, self.downloader)
