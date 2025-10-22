a5a9b74dad6af14451bc1c97ae660a1dfc564e74
12936 <186687011+luoyrair@users.noreply.github.com>
2025-10-22 23:06:41 +0800

    提供了一种在零号空洞-枯萎之都的挑战副本中添加“枯败花圃·核心”和“枯败花圃·苗床”两个副本的方案

--- src/zzz_od/application/hollow_zero/withered_domain/withered_domain_app.py
+++ src/zzz_od/application/hollow_zero/withered_domain/withered_domain_app.py
@@ -115,6 +115,11 @@ class WitheredDomainApp(ZApplication):
     @operation_node(name='选择副本')
     def choose_mission(self) -> OperationRoundResult:
         area = self.ctx.screen_loader.get_area('零号空洞-入口', '副本列表')
+        if self.mission_type_name == "枯败花圃":
+            if self.mission_name == "枯败花圃-探索":
+                self.mission_name = "探索"
+            elif self.mission_name == "枯败花圃-闪击":
+                self.mission_name = "闪击"
         return self.round_by_ocr_and_click(self.last_screenshot, self.mission_name, area=area,
                                            success_wait=1, retry_wait=1)
 
