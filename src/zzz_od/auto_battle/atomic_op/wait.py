34abbda6172f93fc8080613e4d5f02759dcc37c9
Yokami <95584793+kawayiYokami@users.noreply.github.com>
2025-11-15 08:46:36 +0800

    fix: 新架构下，原子操作需要由自己打断

--- src/zzz_od/auto_battle/atomic_op/wait.py
+++ src/zzz_od/auto_battle/atomic_op/wait.py
@@ -1,4 +1,5 @@
 import time
+import threading
 from typing import ClassVar
 
 from one_dragon.base.conditional_operation.atomic_op import AtomicOp
@@ -16,6 +17,12 @@ class AtomicWait(AtomicOp):
                 wait_seconds = float(op_def.data[0])
         AtomicOp.__init__(self, op_name='%s %.2f' % (AtomicWait.OP_NAME, wait_seconds))
         self.wait_seconds: float = wait_seconds
+        self._stop_event = threading.Event()  # 用于中断的Event
 
     def execute(self):
-        time.sleep(self.wait_seconds)
+        # 使用 wait() 而不是 sleep()，这样可以被 stop() 中断
+        self._stop_event.wait(timeout=self.wait_seconds)
+
+    def stop(self):
+        # 中断等待
+        self._stop_event.set()
