b2e6f24198c4bd2c7ab5091d255b518e638767a6
kawayiYokami <289104862@qq.com>
2026-01-20 01:31:14 +0800

    优化仓库扫描界面：删除翻译按钮，添加打开输出目录功能

--- src/zzz_od/gui/view/game_assistant/inventory_scan_interface.py
+++ src/zzz_od/gui/view/game_assistant/inventory_scan_interface.py
@@ -1,17 +1,17 @@
+import os
 from typing import Optional
 
 from PySide6.QtWidgets import QVBoxLayout, QWidget
 from qfluentwidgets import FluentIcon, PushButton
 
 from one_dragon.base.operation.application import application_const
+from one_dragon.utils import os_utils
 from one_dragon_qt.utils.config_utils import get_prop_adapter
 from one_dragon_qt.view.app_run_interface import AppRunInterface
 from one_dragon_qt.widgets.row import Row
 from one_dragon_qt.widgets.setting_card.help_card import HelpCard
-from one_dragon_qt.widgets.setting_card.spin_box_setting_card import DoubleSpinBoxSettingCard
 from zzz_od.application.inventory_scan import inventory_scan_const
 from zzz_od.application.inventory_scan.inventory_scan_config import InventoryScanConfig
-from zzz_od.application.inventory_scan.translation import TranslationUpdater
 from zzz_od.application.zzz_application import ZApplication
 from zzz_od.context.zzz_context import ZContext
 
@@ -43,32 +43,21 @@ class InventoryScanInterface(AppRunInterface):
         self.help_opt = HelpCard(
             url='https://one-dragon.com/zzz/zh/docs/feat_game_assistant.html',
             title='使用说明',
-            content='从大世界开始，自动扫描驱动盘、音擎、角色（音擎和角色暂未实现）'
+            content='从大世界开始，自动扫描驱动盘、音擎、角色，并导出数据到 .debug\\inventory_exports 目录'
         )
         left_layout.addWidget(self.help_opt)
 
-        # 更新翻译字典按钮
-        self.update_dict_btn = PushButton('更新翻译字典', self)
-        self.update_dict_btn.clicked.connect(self._on_update_dict_clicked)
-        left_layout.addWidget(self.update_dict_btn)
+        # 打开输出目录按钮
+        self.open_output_btn = PushButton('打开输出目录', self, FluentIcon.FOLDER)
+        self.open_output_btn.clicked.connect(self._on_open_output_clicked)
+        left_layout.addWidget(self.open_output_btn)
 
         return content
 
-    def _on_update_dict_clicked(self):
-        """更新翻译字典按钮点击事件"""
-        self.update_dict_btn.setEnabled(False)
-        self.update_dict_btn.setText('正在更新...')
-        try:
-            updater = TranslationUpdater()
-            success = updater.update_all()
-            if success:
-                self.update_dict_btn.setText('更新成功')
-            else:
-                self.update_dict_btn.setText('更新失败')
-        except Exception as e:
-            self.update_dict_btn.setText(f'更新失败: {e}')
-        finally:
-            self.update_dict_btn.setEnabled(True)
+    def _on_open_output_clicked(self):
+        """打开输出目录按钮点击事件"""
+        output_dir = os_utils.get_path_under_work_dir('.debug', 'inventory_exports')
+        os.startfile(output_dir)
 
     def on_interface_shown(self) -> None:
         AppRunInterface.on_interface_shown(self)
