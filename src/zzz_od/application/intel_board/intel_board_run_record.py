a418c6207fad8155f4c23fee8b44618ba0a975f9
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2026-02-09 15:31:27 +0800

    fix: 修复run_record

--- src/zzz_od/application/intel_board/intel_board_run_record.py
+++ src/zzz_od/application/intel_board/intel_board_run_record.py
@@ -1,12 +1,13 @@
-from typing import Optional
-
-from one_dragon.base.operation.application_run_record import AppRunRecord, AppRunRecordPeriod
+from one_dragon.base.operation.application_run_record import (
+    AppRunRecord,
+    AppRunRecordPeriod,
+)
 from zzz_od.application.intel_board import intel_board_const
 
 
 class IntelBoardRunRecord(AppRunRecord):
 
-    def __init__(self, instance_idx: Optional[int] = None, game_refresh_hour_offset: int = 0):
+    def __init__(self, instance_idx: int | None = None, game_refresh_hour_offset: int = 0):
         AppRunRecord.__init__(
             self,
             intel_board_const.APP_ID,
@@ -14,6 +15,8 @@ class IntelBoardRunRecord(AppRunRecord):
             game_refresh_hour_offset=game_refresh_hour_offset,
             record_period=AppRunRecordPeriod.WEEKLY
         )
+
+    def reset_record(self):
+        AppRunRecord.reset_record(self)
         self.expert_challenge_count: int = 0
         self.notorious_hunt_count: int = 0
-
