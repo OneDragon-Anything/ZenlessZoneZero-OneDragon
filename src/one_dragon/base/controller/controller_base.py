ecacde19e2d0b543f96bc2266a57187784d27f51
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2026-03-05 07:20:25 +0800

    chore: 添加基类参数

--- src/one_dragon/base/controller/controller_base.py
+++ src/one_dragon/base/controller/controller_base.py
@@ -45,12 +45,13 @@ class ControllerBase:
         """
         return False
 
-    def click(self, pos: Point = None, press_time: float = 0, pc_alt: bool = False) -> bool:
+    def click(self, pos: Point = None, press_time: float = 0, pc_alt: bool = False, gamepad_key: str | None = None) -> bool:
         """
         点击位置
         :param pos: 点击位置 (x,y) 默认分辨率下的游戏窗口里的坐标
         :param press_time: 大于0时长按若干秒
         :param pc_alt: 只在PC端有用 使用ALT键进行点击
+        :param gamepad_key: 只在PC端有用 不响应ALT键时使用虚拟手柄替代进行点击
         :return: 不在窗口区域时不点击 返回False
         """
         pass
