e96f7c4f0e3b758d8c652204bdc23622c62068a8
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2026-03-19 16:16:08 +0800

    revert: 恢复体力计划标签页

--- src/zzz_od/gui/view/one_dragon/zzz_one_dragon_interface.py
+++ src/zzz_od/gui/view/one_dragon/zzz_one_dragon_interface.py
@@ -3,6 +3,7 @@ from qfluentwidgets import FluentIcon
 from one_dragon_qt.widgets.pivot_navi_interface import PivotNavigatorInterface
 from one_dragon_qt.widgets.setting_card.app_run_card import AppRunCard
 from zzz_od.context.zzz_context import ZContext
+from zzz_od.gui.view.one_dragon.charge_plan_interface import ChargePlanInterface
 from zzz_od.gui.view.one_dragon.mouse_sensitivity_checker_interface import MouseSensitivityCheckerInterface
 from zzz_od.gui.view.one_dragon.predefined_team_interface import PredefinedTeamInterface
 from zzz_od.gui.view.one_dragon.zzz_one_dragon_run_interface import ZOneDragonRunInterface
@@ -24,6 +25,7 @@ class ZOneDragonInterface(PivotNavigatorInterface):
 
     def create_sub_interface(self) -> None:
         self.add_sub_interface(ZOneDragonRunInterface(self.ctx))
+        self.add_sub_interface(ChargePlanInterface(self.ctx))
         self.add_sub_interface(PredefinedTeamInterface(self.ctx))
         self.add_sub_interface(MouseSensitivityCheckerInterface(self.ctx))
 
