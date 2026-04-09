6ba17025b44a1dcf44b20e623965faa75a7f18cd
Usagi-wusaqi <222152280+Usagi-wusaqi@users.noreply.github.com>
2026-04-09 18:11:11 +0800

    refactor: 使用文档字符串说明恶名狩猎节点逻辑

--- src/zzz_od/operation/compendium/notorious_hunt.py
+++ src/zzz_od/operation/compendium/notorious_hunt.py
@@ -170,12 +170,16 @@ class NotoriousHunt(ZOperation):
     @node_from(from_name='选择副本')
     @operation_node(name='抉择恶名狩猎', node_max_retry_times=10)
     def decide_notorious_hunt(self) -> OperationRoundResult:
+        """
+        恶名狩猎周期挑战
+
+        出现'深度追猎-信息'，说明周期挑战'剩余次数'已经用完，直接标记完成
+        通过识别剩余奖励次数决定本次可运行次数，周期挑战不用消耗体力
+        """
+
         if self.use_charge_power:
             return self.round_success('深度追猎')
 
-        # 恶名狩猎 app: 不用消耗体力(每周刷新三次免费的剩余奖励次数)
-        # 存在"深度追猎-信息", 则没有剩余奖励次数, 标记完成
-        # "剩余次数"可以读取, 通过识别结果决定具体运行几次
         result = self.round_by_find_area(self.last_screenshot, '恶名狩猎', '深度追猎-信息')
         if result.is_success:
             self.run_record.left_times = 0
@@ -205,15 +209,17 @@ class NotoriousHunt(ZOperation):
     @node_from(from_name='抉择恶名狩猎', status='深度追猎')
     @operation_node(name='抉择深度追猎')
     def decide_by_use_power(self) -> OperationRoundResult:
-        # 恶名狩猎 深度追猎: 需要消耗体力(入口要在剩余奖励次数用完才会开启)
-        # 没有"深度追猎-信息", 则还有剩余奖励次数, 立即跳过
-        # 区域对应文本"剩余奖励次数"变成"电量消耗", 导致"剩余次数"无法读取
+        """
+        恶名狩猎深度追猎
+
+        出现'深度追猎-信息'，且点亮'深度追猎-ON'后，才可正式开始
+        区域对应文本"剩余奖励次数"会变成"电量消耗"，深度追猎需要消耗体力
+        """
 
         # 尝试点亮"深度追猎-ON"之后的对话框
         result = self.round_by_find_and_click_area(self.last_screenshot, '恶名狩猎', '按钮-深度追猎-确认')
         if result.is_success:
             return self.round_wait(result.status, wait=1)
-        # 尝试识别"深度追猎-信息"是否存在, 然后点亮"深度追猎-ON"以正式开启深度追猎
         result = self.round_by_find_area(self.last_screenshot, '恶名狩猎', '深度追猎-信息')
         if result.is_success:
             result = self.round_by_find_area(self.last_screenshot, '恶名狩猎', '按钮-深度追猎-ON')
