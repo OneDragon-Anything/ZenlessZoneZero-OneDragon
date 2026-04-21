77ec8b5866f80fc02eadae2ef74e818efce5a182
Josh Cai <joshcai_mdwechat@163.com>
2026-04-21 19:25:16 +0800

    使用模板匹配替代区域硬编码, 同时滑动使用screen_utils中的scroll_area方法

--- src/zzz_od/operation/agent_template_matcher.py
+++ src/zzz_od/operation/agent_template_matcher.py
@@ -1,3 +1,6 @@
+from cv2.typing import MatLike
+
+from one_dragon.base.geometry.rectangle import Rect
 from one_dragon.base.matcher.match_result import MatchResult
 from one_dragon.utils import cv2_utils
 from zzz_od.context.zzz_context import ZContext
@@ -6,14 +9,17 @@ from zzz_od.game_data.agent import Agent, AgentEnum
 
 def match_team_agent_template(
     ctx: ZContext,
-    source_kp,
-    source_desc,
-    agent_id_list: list[str] | None = None,
+    screen: MatLike,
+    rect: Rect,
+    agent_list_str: list[str] | None = None
 ) -> list[MatchResult]:
+    part = cv2_utils.crop_image_only(screen, rect)
+    source_kp, source_desc = cv2_utils.feature_detect_and_compute(part)
+
     agent_mr_list: list[MatchResult] = []
     for agent_enum in AgentEnum:
         agent: Agent = agent_enum.value
-        if agent_id_list is not None and agent.agent_id not in agent_id_list:
+        if agent_list_str is not None and agent.agent_id not in agent_list_str:
             continue
         for template_id in agent.template_id_list:
             template = ctx.template_loader.get_template('predefined_team', f'avatar_{template_id}')
@@ -33,6 +39,7 @@ def match_team_agent_template(
             if mr is None:
                 continue
 
+            mr.add_offset(rect.left_top)
             agent_mr = mr
             agent_mr.data = agent
             agent_mr_list.append(agent_mr)
