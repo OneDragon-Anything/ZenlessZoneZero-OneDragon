7b27284b0ed933d4f7ebefe661624d6467ae3cf8
Josh Cai <joshcai_mdwechat@163.com>
2026-04-26 21:14:16 +0800

    添加函数类型签名

--- src/one_dragon/utils/gpu_executor.py
+++ src/one_dragon/utils/gpu_executor.py
@@ -1,12 +1,16 @@
 from concurrent.futures import ThreadPoolExecutor, Future
-
+from collections.abc import Callable
+from typing import ParamSpec, TypeVar
 from one_dragon.utils import thread_utils
 
 # 限制只能有一个方法访问gpu 避免gpu资源竞争崩溃
 _executor = ThreadPoolExecutor(thread_name_prefix='od_gpu', max_workers=1)
 
+P = ParamSpec('P')
+R = TypeVar('R')
 
-def submit(fn, /, *args, **kwargs) -> Future:
+
+def submit(fn: Callable[P], /, *args: P.args, **kwargs: P.kwargs) -> Future:
     f = _executor.submit(fn, *args, **kwargs)
     f.add_done_callback(thread_utils.handle_future_result)
 
@@ -14,7 +18,7 @@ def submit(fn, /, *args, **kwargs) -> Future:
 
 
 # 给 submit 包一层, 以简化调用写法
-def execute_function(is_use_gpu, fn, /, *args, **kwargs):
+def execute_function(is_use_gpu, fn: Callable[P, R], /, *args: P.args, **kwargs: P.kwargs) -> R:
     if is_use_gpu:
         f = submit(fn, *args, **kwargs)
         return f.result()
