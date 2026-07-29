cec94f6c9131c656c529c1e6614f3af864442940
Josh Cai <joshcai_mdwechat@163.com>
2026-07-29 20:03:07 +0800

    删除返回类型

--- src/zzz_od/operation/enter_game/enter_game.py
+++ src/zzz_od/operation/enter_game/enter_game.py
@@ -75,7 +75,6 @@ class EnterGame(ZOperation):
         if self.ctx.game_account_config.game_region == GameRegionEnum.CNB.value.value:
             log.info('检测到B服登录, 使用Bitblt截图方式')
             self.ctx.controller.screenshot_controller.init_screenshot(ScreenshotMethodEnum.BITBLT.value.value)
-        return self.round_success()
 
     @node_from(from_name='国服-输入账号密码')
     @node_from(from_name='国服-输入账号密码-新')
